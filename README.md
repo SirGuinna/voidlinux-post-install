95b3fd2370664faccade3b47735ef33c8317f61b
SirGuinna <noanpires@gmail.com>
2024-11-13 16:31:20 -0300

    .

---
+++ README.md
@@ -0,0 +1,136 @@
+<p align="center">
+<img width="700px" src="https://github.com/SirGuinna/voidlinux-post-install/blob/main/img/void-img.jpg" align="center" alt="white" /><br><br>
+  
+<img alt="Maintained" src="https://img.shields.io/badge/Maintained%3F-Yes-green">
+<img alt="GitHub Last Commit" src="https://img.shields.io/github/last-commit/SirGuinna/voidlinux-post-install">
+</p>
+
+# AVISO
+
+Ao usar este roteiro você assume que entende os riscos e assume total responsabilidade por suas ações. Não há nenhuma garantia implícita ou explícita do seu funcionamento. O intuito é facilitar a instalação, resolvi deixar mais completo caso alguem queira aprender ou usar de forma manual. Se ele te ajudar deixe uma Estrela.
+
+# Objetivos
+
+Neste roteiro considero que estamos partindo de uma instalação BASE do **VOID LINUX**. Esse roteiro funciona como um guia passo a passo para ajudar na **PÓS-INSTALAÇÃO** e configuração usando o I3WM como interface gráfica.
+<br>
+O objetivo deste roteiro **não é ser um script totalmente automatizado**, apesar de no fim desse documento ter um Script para automatizar esse processo caso queira utilizar.
+<br>
+A seleção de pacotes escolhidos, foi feita para atender instalações em DESKTOPs e NOTEBOOKs, então remova ou adicione pacotes de acordo com sua necessidade e escolha. **NO FIM DESSE DOCUMENTO TEM UM SCRIPT PARA AUTOMATIZAR TODO ESSE PROCESSO**.
+
+# 1º - Preparação
+
+## 1.1 Conectando na Internet
+WIFI CONNECTION
+```shellscript
+sudo ip link set wlo1 up
+```
+```shellscript
+wpa_passphrase "SSID" "password" | sudo tee /etc/wpa_supplicant.conf
+```
+```shellscript
+sudo wpa_supplicant -B -i wlo1 -c /etc/wpa_supplicant.conf
+```
+```shellscript
+sudo dhclient wlo1
+```
+<br>
+ETHERNET CONNECTION
+```shellscript
+ip link
+```
+```shellscript
+sudo ip link set YOUR_CONNECTION up
+```
+```shellscript
+sudo dhclient YOUR_CONNECTION
+```
+
+## 1.2 Atualizando Sistema
+```shellscript
+sudo xbps-install -Syu
+```
+
+## 1.3 Instalando Pacotes Essenciais
+```shellscript
+sudo xbps-install -S git xorg lightdm i3 xfce4-terminal nano xed rofi firefox sysstat i3blocks curl lxappearance nitrogen lxqt-policykit xfce4-power-manager lightdm-webkit2-greeter psmisc xdotool volumeicon notify-osd pavucontrol which mpv htop thunar fastfetch w3m acpid fish arandr networkmanager elogind picom font-awesome noto-fonts-emoji
+```
+
+## 1.4 Instalando Suporte a Flatpak (Opcional)
+```shellscript
+sudo xbps-install -S flatpak
+```
+```shellscript
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+```
+
+# 2º - Personalização do Ambiente I3WM
+
+## 2.1 Copiando Configurações Prontas (Opcional)
+```shellscript
+git clone https://github.com/SirGuinna/voidlinux-post-install;cd voidlinux-post-install
+```
+```shellscript
+mkdir -p $HOME/.config
+```
+```shellscript
+cp -vf ./compton.conf $HOME/.config/
+```
+```shellscript
+cp -vf ./picom.conf $HOME/.config/
+```
+```shellscript
+fc-cache -fv
+```
+```shellscript
+cp -vf ./.i3blocks.conf $HOME/
+```
+```shellscript
+cp -rvf ./i3 $HOME/.config/
+```
+```shellscript
+sudo cp -rvf ./rofi-themes/* /usr/share/rofi/themes/
+```
+
+## 2.2 Instalando Fish Shell (Opcional)
+```shellscript
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+```
+
+## 2.3 Recarrega i3 para aplicar mudanças
+```shellscript
+sudo i3-msg reload > /dev/null
+```
+
+## 2.4 Ativando inicialização de serviços junto com o sistema
+```shellscript
+sudo ln -sf /etc/sv/networkmanager /var/service
+```
+```shellscript
+sudo ln -sf /etc/sv/dbus /var/service
+```
+```shellscript
+sudo ln -sf /etc/sv/lightdm /var/service
+```
+```shellscript
+sudo ln -sf /etc/sv/elogind /var/service
+```
+```shellscript
+sudo ln -sf /etc/sv/acpid /var/service
+```
+
+
+---
+
+
+## Script de Instalação Automática
+```shellscript
+git clone https://github.com/SirGuinna/voidlinux-post-install;cd voidlinux-post-install;chmod +x void-installer.sh;./void-installer.sh
+```
+
+## Use o theme-selector.py para escolher um tema predefinido
+```shellscript
+cd voidlinux-post-install
+```
+```shellscript
+theme-selector.py
+```
---
+++ void-installer.sh
@@ -0,0 +1,168 @@
+#!/bin/bash
+
+CONFIG_DIR="$HOME/.config"
+ROFI_THEME_DIR="/usr/share/rofi/themes"
+
+# Verifica se o script está sendo executado com permissões de root
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Este script precisa ser executado como ROOT."
+    echo "This script needs to be run as ROOT."
+    exit 1
+fi
+
+# Habilita o modo de falha imediata (interrompe o script se qualquer comando falhar)
+set -e
+
+# Verifica a conexão com a Internet
+verificar_conexao() {
+    echo "Checking Internet Connection..."
+    if ping -q -c 1 -W 1 google.com >/dev/null; then
+        echo "Internet Connection Verified."
+    else
+        echo "ERROR: Sem conexão com a internet. Conecte-se e tente novamente."
+        echo "ERROR: No internet connection. Connect and try again."
+        echo "X"
+        echo "# # # # # # # # # # # # # # WIFI CONNECTION # # # # # # # # # # # # # # #"
+        echo "#                       sudo ip link set wlo1 up                        #"
+        echo "#  wpa_passphrase "SSID" "password" | sudo tee /etc/wpa_supplicant.conf #"
+        echo "#       sudo wpa_supplicant -B -i wlo1 -c /etc/wpa_supplicant.conf      #"
+        echo "#                          sudo dhclient wlo1                           #"
+        echo "# # # # # # # # # # # # # # # # # # # ! # # # # # # # # # # # # # # # # #"
+        echo "X"
+        echo "# # # # # # # # # # # # # ETHERNET CONNECTION # # # # # # # # # # # # # #"
+        echo "#                              ip link                                  #"
+        echo "#                  sudo ip link set YOUR_CONNECTION up                  #"
+        echo "#                     sudo dhclient YOUR_CONNECTION                     #"
+        echo "# # # # # # # # # # # # # # # # # # # ! # # # # # # # # # # # # # # # # #"
+        while true; do
+            read -p "Press Enter to try again or Ctrl+C to exit..."
+            if ping -q -c 1 -W 1 google.com >/dev/null; then
+                echo "Internet Connection Verified."
+                break
+            else
+                echo "No connection. Please try again."
+            fi
+        done
+    fi
+}
+
+# Atualização completa do sistema
+atualizar_sistema() {
+    echo "The system will update, it will take a few minutes..."
+    sudo xbps-install -Syu
+    echo "System updated successfully!"
+}
+
+# Funcao para instalar o Flatpak e adicionar o repositorio Flathub
+instalar_flatpak_e_adicionar_flathub() {
+    # Verifica e instala o Flatpak
+    if ! command -v flatpak &> /dev/null; then
+        echo "Flatpak is not installed. Installing Flatpak..."
+        sudo xbps-install -y flatpak
+    fi
+    # Verifica e adiciona o repositorio Flathub se ainda nao estiver adicionado
+    if ! flatpak remote-list | grep -q flathub; then
+        echo "Adding Flathub repository..."
+        flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    else
+        echo "Flathub repository already added."
+    fi
+}
+
+# Instalação dos pacotes necessários
+instalar_pacotes() {
+    echo "Starting to install packages will take a few minutes..."
+    sudo xbps-install -y git \
+        xorg \
+        lightdm \
+        i3 \
+        xfce4-terminal \
+        nano \
+        xed \
+        rofi \
+        firefox \
+        sysstat \
+        i3blocks \
+        curl \
+        lxappearance \
+        nitrogen \
+        lxqt-policykit \
+        xfce4-power-manager \
+        lightdm-webkit2-greeter \
+        psmisc \
+        xdotool \
+        volumeicon \
+        notify-osd \
+        pavucontrol \
+        which \
+        mpv \
+        htop \
+        thunar \
+        fastfetch \
+        w3m \
+        acpid \
+        fish \
+        arandr \
+        networkmanager \
+        elogind \
+        picom \
+        font-awesome \
+        noto-fonts-emoji
+    echo "Additional packages installed successfully!"
+}
+
+# Copia configurações personalizadas, substituindo arquivos existentes caso tenha
+copiar_configs() {
+    echo "Copying configuration files..."
+    
+    mkdir -p "$CONFIG_DIR"
+    cp -vf ./compton.conf "$CONFIG_DIR/"
+    cp -vf ./picom.conf "$CONFIG_DIR/"
+
+    # Atualiza cache de fontes
+    fc-cache -fv
+
+    # Copia configuração do i3blocks e temas do rofi, substituindo arquivos existentes caso tenha
+    cp -vf ./.i3blocks.conf "$HOME/"
+    cp -rvf ./i3 "$CONFIG_DIR/"
+    cp -rvf ./rofi-themes/* "$ROFI_THEME_DIR/"
+    
+    echo "Copies made successfully!"
+}
+
+# Instalação do Oh-My-Fish para o Fish Shell
+instalar_fish_shell() {
+    echo "Installing Fish Shell..."
+    curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+    echo "Instalation made successfully!"
+}
+
+# Ativa serviços essenciais
+ativar_servicos() {
+    echo "Iniciando serviços essenciais junto com o sistema..."
+    sudo ln -sf /etc/sv/networkmanager /var/service
+    sudo ln -sf /etc/sv/dbus /var/service
+    sudo ln -sf /etc/sv/lightdm /var/service
+    sudo ln -sf /etc/sv/elogind /var/service
+    sudo ln -sf /etc/sv/acpid /var/service
+
+    # Recarrega o i3 para aplicar mudanças
+    sudo i3-msg reload > /dev/null
+}
+
+## ICONES para i3blocks -> https://fontawesome.com/v5/cheatsheet
+## Alternativas para notificação pesquisar -> notification-daemon
+
+#####################################################################################################
+
+# Inicio do processo
+echo "Starting the installation process..."
+
+# Chamando as funções
+verificar_conexao
+atualizar_sistema
+instalar_flatpak_e_adicionar_flathub
+instalar_pacotes
+copiar_configs
+instalar_fish_shell
+ativar_servicos
